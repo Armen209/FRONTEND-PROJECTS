4f4eaafa881766504a00df699e4c7478b92881a4
Armen209 <gevorgyanarmen05@gmail.com>
2026-09-22 18:11:58 +0400

    Delete social-network directory

--- social-network/frontend/src/App.tsx
+++
@@ -1,7 +0,0 @@
-export default function App(){
-  return (
-    <div>
-      <h2 className="text-red-400">wellcome</h2>
-    </div>
-  )
-}
--- social-network/frontend/src/helpers/api.ts
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-import { BASE } from "./constants";
-
-export const Http = axios.create({
-    baseURL:BASE
-})
